3c34536d0f18887f51d3b099bd1a18bbabb223aa
joel odunoye <joelodunoye@gmail.com>
2025-11-11 07:47:25 +0100

    update batch processing script

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-import mysql.connector
 from seed import connect_to_prodev
 
 
 def stream_users_in_batches(batch_size):
-    """Fetch rows from user_data in batches using yield"""
+    """Generator that fetches rows in batches using yield"""
     connection = connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT * FROM user_data")
@@ -13,19 +12,18 @@ def stream_users_in_batches(batch_size):
     for row in cursor:
         batch.append(row)
         if len(batch) == batch_size:
-            yield batch
+            yield batch      # YIELD keyword (not return)
             batch = []
 
-    # yield any remaining users
     if batch:
-        yield batch
+        yield batch          # YIELD remaining records
 
     cursor.close()
     connection.close()
 
 
 def batch_processing(batch_size):
-    """Process each batch to filter users over age 25"""
+    """Processes each batch to filter users over age 25"""
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user['age'] > 25:
