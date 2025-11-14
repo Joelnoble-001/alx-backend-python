7a0b88a3b7fdc46403b8b13269c94a16a358d28e
joel odunoye <joelodunoye@gmail.com>
2025-11-14 09:07:18 +0100

    update 1-execute.py

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -1,21 +1,32 @@
 import sqlite3
 
 class ExecuteQuery:
-    def __init__(self, db_name, query, params=None):
-        self.db_name = db_name
+    def __init__(self, query, params=None):
         self.query = query
-        self.params = params if params else ()
+        self.params = params
         self.conn = None
-        self.result = None
+        self.cursor = None
 
     def __enter__(self):
-        self.conn = sqlite3.connect(self.db_name)
-        cursor = self.conn.cursor()
-        cursor.execute(self.query, self.params)
-        self.result = cursor.fetchall()
-        return self.result     # returned inside WITH block
+        self.conn = sqlite3.connect("users.db")
+        self.cursor = self.conn.cursor()
+
+        # Execute the query when entering
+        if self.params:
+            self.cursor.execute(self.query, self.params)
+        else:
+            self.cursor.execute(self.query)
+
+        return self.cursor.fetchall()
 
     def __exit__(self, exc_type, exc_value, traceback):
         if self.conn:
             self.conn.close()
-        return False   # Do not suppress exceptions
+        # Returning False allows exceptions to propagate
+        return False
+
+query = "SELECT * FROM users WHERE age > ?"
+params = (25,)
+
+with ExecuteQuery(query, params) as results:
+    print(results)
