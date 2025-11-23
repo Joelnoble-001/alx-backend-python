5634308f20f47fe778ab75972624c9c202482248
joel odunoye <joelodunoye@gmail.com>
2025-11-23 18:00:44 +0100

    more auth added to messaging app

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,15 +1,11 @@
+from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 
-class IsOwner(BasePermission):
-    """
-    Allows access only to objects owned by the requesting user.
-    """
 
+class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if hasattr(obj, "user"):
             return obj.user == request.user
-        
         if hasattr(obj, "users"):
             return request.user in obj.users.all()
-        
         return False
